a90360f8910562cdc051e961bc743ac6618f13ee
vuquangtuan123 <vuquangtuan12345@gmail.com>
2016-08-02 19:20:07 +0700

    Modify hang_rong.py

--- hang_rong.py
+++ hang_rong.py
@@ -1,59 +1,72 @@
+#coding:utf-8
 import random
 import time
 
 def chon_cap_do():
-    print 'Chon cap do :'
-    print '(de = 1 ; trung binh = 2 ; kho = 3 )'
+    print '\nChọn cấp độ :'
+    print '(Dễ = 1 ; Trung bình = 2 ; Khó = 3 )'
     cap_do = raw_input()
 
     if cap_do == '1':
-        print 'Ban da chon cap do de'
+        print 'Bạn đã chọn cấp độ Dễ'
         tong_hang = '2'
+        
+    elif cap_do == '3':
+        print 'Bạn đã chọn cấp độ Khó'
+        tong_hang = '4'
+
+    else:
+        print 'Bạn đã chọn cấp độ Trung bình'
+        tong_hang = '3'
+
     return tong_hang
 
-def hien_gioi_thieu(hang1):
-    print 'Ban dang song o mot vung dat cua rong. Truoc mat ban'
-    print ('la ' + hang + ' cai hang dong lon. 1 cai la noi o cua con rong')
-    print 'rat than thien, no se chia se cho ban kho bau cua no.'
-    print 'Con lai la noi o cua nhung con rong rat tham lam va '
-    print 'khat mau, chung se an tuoi nuot song ban... '
+def hien_gioi_thieu(so_hang_gioi_thieu):
+    print '\nBạn đang sống ở 1 vùng đất của rồng. Trước mắt bạn'
+    print ('là ' + so_hang_gioi_thieu + ' cái hang động lớn. 1 cái là nơi ở của con rồng ')
+    print 'thân thiện, nó sẻ chia sẻ kho báu của nó cho bạn. '
+    print 'Còn lại là nơi ở của những con rồng tham lam, độc ác '
+    print 'và rất khát máu, chúng sẽ ăn tươi nuốt sống bạn... '
 
-def chon_hang_rong():
+def chon_hang_rong(so_hang_gioi_thieu):
     hang = ''
-    while hang != '1' and hang != '2' and hang != '3' and hang != '4':
-        print 'Hay chon hang dong (1 / 2 / 3)'
+    tong_hang = [str(x + 1) for x in range(int(so_hang_gioi_thieu))]
+    chuoi_hang = ' / '.join(tong_hang)
+    while hang not in tong_hang:
+        print ('Hay chon hang dong ' + chuoi_hang)
         hang = raw_input()
 
     return hang
 
-def kiem_tra_hang(hang_da_chon):
-    print 'Ban tu tu tien vao trong hang ...'
-    
+def kiem_tra_hang(hang_da_chon, so_hang_kiem_tra):
+    print 'Bạn từ từ tiến vào trong hang...'
+
     time.sleep(3)
-    
-    print 'Trong hang rat toi va dang so...'
-    
+
+    print 'Bên trong hang rất tối và đáng sợ...'
+
     time.sleep(2)
-    
-    print 'Bong nhien mot con rong bay den truoc va ban , mo rong'
-    print 'quai ham va ...'
-    
-    time.sleep(2)
-    
-    rong_tot = random.randint(1, 3)
+
+    print 'Bỗng nhiên một con rồng bay đến trước mặt bạn, mở rộng '
+    print 'quai hàm và ...'
+
+    time.sleep(3)
+
+    rong_tot = random.randint(1, int(so_hang_kiem_tra))
 
     if hang_da_chon == str(rong_tot):
-        print 'chia se cho ban kho bau cua no.'
+        print 'chia sẻ cho bạn kho báu của nó.'
 
     else:
-        print 'nuot chung ban.'
+        print 'nuốt chửng bạn.'
 
 dong_y = 'co'
 while dong_y == 'co' or dong_y == 'y' or dong_y == 'yes' or dong_y == 'ok':
-    hang = chon_cap_do() 
+    hang = chon_cap_do()
+    #kiem_tra_so_hang = chon_cap_do()
     hien_gioi_thieu(hang)
-    so_hang = chon_hang_rong()
-    kiem_tra_hang(so_hang)
+    so_hang = chon_hang_rong(hang)
+    kiem_tra_hang(so_hang, hang)#kiem_tra_so_hang)
 
-    print ('Ban co muon choi lai? (co/khong)')
+    print ('Bạn có muốn chơi lại? (co/khong)')
     dong_y = raw_input()
